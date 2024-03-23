8eb4f5cac6f364ab1236f648a7b3e0072168839f
TannerParks <59822998+TannerParks@users.noreply.github.com>
2024-03-23 11:25:16 -0700

    Snake functions decoupled and ready for AI

--- SnakeGame.py
+++ SnakeGame.py
@@ -5,13 +5,12 @@ import random
 from math import dist
 import numpy as np
 
-import Human.HumanController as HumanController
-
+# Colors
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
-BLOCK = 20  # Size of a block
+BLOCK_SIZE = 20  # Size of a block
 SPEED = 20  # Speed of the game
 
 WIDTH = 600 # Width and height of the window
@@ -27,18 +26,18 @@ class Fruit:
         self.snake = snake  # Reference to the snake object, ensures the Fruit classs has the most up-to-date snake position
         self.x = None
         self.y = None
-        self.move()
+        self.move_fruit()
 
     def draw_fruit(self):
         """Draws the fruit on the board."""
-        pygame.draw.rect(self.window, RED, [self.x, self.y, BLOCK, BLOCK])
+        pygame.draw.rect(self.window, RED, [self.x, self.y, BLOCK_SIZE, BLOCK_SIZE])
         #print(self.snake.x, self.snake.y)
 
-    def move(self):
+    def move_fruit(self):
         """Moves the fruit to a new location. If the location is occupied, move to a new location. This is done by generating a 
         list of all possible points and removing the ones that are occupied by the snake. Then, a random point is chosen from the 
         remaining points. We use this in favor of a while loop to prevent the game from freezing if the snake is too long."""
-        all_points = [(x, y) for x in range(0, WIDTH, BLOCK) for y in range(0, HEIGHT, BLOCK)]
+        all_points = [(x, y) for x in range(0, WIDTH, BLOCK_SIZE) for y in range(0, HEIGHT, BLOCK_SIZE)]
         snake_positions = set(zip(self.snake.x, self.snake.y))
         free_points = [pt for pt in all_points if pt not in snake_positions]
 
@@ -84,15 +83,20 @@ class Snake:
         """Draws the snake."""
         self.window.fill(BLACK)
         for i in range(self.length):
-            pygame.draw.rect(self.window, BLACK, [self.x[i], self.y[i], BLOCK, BLOCK])  # Outlines snake segments
+            pygame.draw.rect(self.window, BLACK, [self.x[i], self.y[i], BLOCK_SIZE, BLOCK_SIZE])  # Outlines snake segments
             pygame.draw.rect(self.window, GREEN, [self.x[i] + 2, self.y[i] + 2, 16, 16])
 
+    # FIXME: Can remove this function and just call move_snake() or AI_update_direction directly
     def slither(self, action):
-        """Action tells the snake which direction to go dependent on the game state."""
-        directions = ["right", "down", "left", "up"]  # Clockwise so we can make turns easier below
+        self.AI_update_direction(action)
+        self.move_snake()
+    
+    def AI_update_direction(self, action: list[int]):
+        """Updates the direction of the snake based on the action taken."""
+        directions = ["right", "down", "left", "up"]
         idx = directions.index(self.direction)
 
-        # Change the direction based on the action                          # FIXME: AI input, move to a different function
+        # Change the direction based on the action
         match action:
             case [1, 0, 0]:  # Continue in same direction
                 self.direction = directions[idx]
@@ -104,25 +108,30 @@ class Snake:
                 self.direction = directions[(idx - 1) % 4]
                 #print("left")
             case _:
-                pass # TODO: Human input
+                pass
 
-        for i in range(self.length - 1, 0, -1):  # shift blocks when direction is changed
+    def move_snake(self):
+        """Moves the snake by updating the x and y coordinates of each block. The head of the snake moves in the direction it is going."""
+        # Shift blocks to position of the block in front of it
+        for i in range(self.length - 1, 0, -1):
             self.x[i] = self.x[i - 1]
             self.y[i] = self.y[i - 1]
 
+        # Move the head of the snake in the direction it is facing
         match self.direction:
             case "up":
-                self.y[0] -= BLOCK
+                self.y[0] -= BLOCK_SIZE
             case "down":
-                self.y[0] += BLOCK
+                self.y[0] += BLOCK_SIZE
             case "left":
-                self.x[0] -= BLOCK
+                self.x[0] -= BLOCK_SIZE
             case "right":
-                self.x[0] += BLOCK
+                self.x[0] += BLOCK_SIZE
 
 
 class Game:
-    def __init__(self):
+    def __init__(self, human=True):
+        self.human = human
         self.init_game()
 
     def init_game(self):
@@ -160,12 +169,15 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-                self.force_quit = True
+                #self.force_quit = True
             elif event.type == pygame.KEYDOWN:
-                self.handle_keydown(event.key)
+                # If the game is being played by a human, handle key presses
+                if self.human:
+                    self.handle_keydown(event.key)
 
     def handle_keydown(self, key):
         """Handles key presses."""
+        global SPEED 
         match key:
             case pygame.K_UP:
                 self.snake.move_up()
@@ -176,12 +188,18 @@ class Game:
             case pygame.K_RIGHT:
                 self.snake.move_right()
             case pygame.K_ESCAPE:
-                # self.running = False
-                self.force_quit = True
+                self.running = False
+                #self.force_quit = True
+            case pygame.K_SPACE:    # TODO: Just for debugging purposes
+                if SPEED == 20:
+                    SPEED = 5
+                else:
+                    SPEED = 20
     
-    def update_game_state(self, action):
-        """Updates the game state based on the action taken."""
-        self.snake.slither(action)
+    def update_game_state(self):
+        """Updates the game state by moving the snake, drawing the snake and fruit, and displaying the score."""
+        #self.snake.slither(action)
+        self.snake.move_snake()
         self.snake.draw_snake()
         self.fruit.draw_fruit()
         self.display_score()
@@ -194,24 +212,24 @@ class Game:
     def process_ate_fruit(self):
         """Processes the snake eating a fruit by increasing the score, length, and moving the fruit."""
         self.reward = 50
-        self.dist_to_fruit = math.inf
         self.score += 1
         self.snake.grow()  # Add a block to the snake
-        self.fruit.move()
+        self.fruit.move_fruit()
         
-    def collision(self):    # FIXME: Needs a point so the AI can check for collisions on next move
-        """Checks if the snake has collided with anything by making a set of the snake's body and checking if the head is in it or 
-        checking if the head is out of bounds"""
-        head = Point(self.snake.x[0], self.snake.y[0])
+    def collision(self, point=None):
+        """Checks if a point has collided with anything by making a set of the snake's body and checking if the point is in it or 
+        checking if the point is out of bounds"""
+        if point is None:
+            point = Point(self.snake.x[0], self.snake.y[0]) # Default to the head of the snake
         snake_body = set(zip(self.snake.x[1:], self.snake.y[1:]))
 
-        if (head.x > WIDTH - BLOCK) or (head.x < 0) or (head.y < 0) or (head.y > HEIGHT - BLOCK):
+        if (point.x > WIDTH - BLOCK_SIZE) or (point.x < 0) or (point.y < 0) or (point.y > HEIGHT - BLOCK_SIZE):
             print("Hit wall")
-            return True
-        if head in snake_body:
+            return 1
+        if point in snake_body:
             print("Hit snake")
-            return True
-        return False
+            return 1
+        return 0
     
     def process_collision(self):
         """Processes the snake colliding with something."""
@@ -224,11 +242,16 @@ class Game:
             print("Took too long")
             self.running = False
 
-    def play(self, action):
+    def play(self, action=None):
         """Starts running the base of the game and allows for key presses."""
         self.frame_iteration += 1
         self.handle_events()
-        self.update_game_state(action)
+
+        # If the AI is playing, it will pass in an action
+        if action is not None:
+            self.snake.AI_update_direction(action)
+
+        self.update_game_state()
 
         # Check for collisions and if the snake ate a fruit
         if self.collision():
@@ -237,17 +260,16 @@ class Game:
             self.process_ate_fruit()
             
         # End game after a certain amount of time to encourage faster solutions
-        self.took_too_long()
+        #self.took_too_long()   # FIXME: AI only
 
         self.clock.tick(SPEED)
-        #print(self.reward)
 
         return self.reward, self.running, self.score
 
 
 if __name__ == "__main__":
     game = Game()
-    while game.running and not game.force_quit:
-        game.play(None)
+    while game.running:
+        game.play()
 
     pygame.quit()
